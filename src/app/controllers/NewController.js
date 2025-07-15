31744859548d36db3ac36fa8bbafc24035bd6041
datpd <datpd@enao.local>
2025-07-15 18:24:22 +0700

    install husky, lint-staged

--- src/app/controllers/NewController.js
+++ src/app/controllers/NewController.js
@@ -1,12 +1,12 @@
 class NewController {
-  // [GET] /news
-  index(req, res) {
-    res.render("news");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
 
-  show(req, res) {
-    res.send("DETAIL");
-  }
+    show(req, res) {
+        res.send('DETAIL');
+    }
 }
 
 module.exports = new NewController();
--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const { engine } = require("express-handlebars");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
 app.use(
   express.urlencoded({
     extended: true,
-  })
+  }),
 );
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.engine(
-  "hbs",
+  'hbs',
+
   engine({
-    extname: ".hbs",
-  })
+    extname: '.hbs',
+  }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
