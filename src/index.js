ee3f7fa9d837ef531192d56de09b4c792aee01fd
datpd <datpd@enao.local>
2025-07-15 17:48:45 +0700

    feat(base): create router, app/controller

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 app.engine(
   "hbs",
@@ -19,13 +28,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const newRouter = require("./news");
+const siteRouter = require("./site");
+
+function route(app) {
+  app.use("/", siteRouter);
+  app.use("/news", newRouter);
+  app.use("/search", siteRouter);
+}
+
+module.exports = route;
